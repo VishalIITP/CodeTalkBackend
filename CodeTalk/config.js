6b8f71a92e4fbac13ac5f195eb1affc8d3a02b49
Vishal Yadav <76230753+VishalIITP@users.noreply.github.com>
2023-08-02 19:11:54 +0530

    connected with atlas
    connected with atlas

---
+++ CodeTalk/config.js
@@ -0,0 +1,8 @@
+
+const config={
+    PORT:process.env.PORT || 5000,
+    mongoDBurl:"mongodb+srv://suppourtcodetalk:MZ2UYLfdUJ6lqisn@cluster0.6d3fsx2.mongodb.net/?retryWrites=true&w=majority&ssl=true"
+
+}
+
+module.exports=config;
--- config.js
+++
@@ -1,8 +0,0 @@
-
-const config={
-    PORT:process.env.PORT || 5000,
-    mongoDBurl:"mongodb://127.0.0.1:27017/RegisterdStuds"
-
-}
-
-module.exports=config;
