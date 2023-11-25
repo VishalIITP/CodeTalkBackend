62ef71ae66e7517b4e485b55586303751e7805d0
Vishal Yadav <76230753+VishalIITP@users.noreply.github.com>
2023-11-25 16:56:46 +0530

    added models for editing features

--- CodeTalk/Model/db.js
+++ CodeTalk/Model/db.js
@@ -10,7 +10,7 @@ const studschema = new mongoose.Schema({
   Email: { type: String },
   Phone: { type: String },
   RefrrelCodeApplied: { type: String },
-  ReferredBy:{type:String},
+  ReferredBy: { type: String },
   Time: { type: String },
   VPA: { type: String },
   PTime: { type: String }
@@ -22,8 +22,40 @@ const feedbackschema = new mongoose.Schema({
   Feedback: { type: String }
 });
 
+const newWebsiteschema = new mongoose.Schema({
+  websiteName: { type: String },
+  isActive: { type: Boolean },
+  heroT1U: { type: String },
+  heroT1D: { type: String },
+  heroT2U: { type: String },
+  heroT2D: { type: String },
+  date: { type: String },
+  time: { type: String },
+  price: { type: String },
+  strikePrice: { type: String },
+  timer: { type: String },
+  wthc1: { type: String },
+  wthc2: { type: String },
+  wttp: {type: String},
+  Wtdp1h: { type: String },
+  wtdp1c: { type: String },
+  Wtdp2h: { type: String },
+  wtdp2c: { type: String },
+  Wtdp3h: { type: String },
+  wtdp3c: { type: String },
+  Wtdp4h: { type: String },
+  wtdp4c: { type: String },
+  Wtdp5h: { type: String },
+  wtdp5c: { type: String },
+  Wtdp6h: { type: String },
+  wtdp6c: { type: String },
+
+
+});
+
 const StudentsModel = mongoose.model("students", studschema);
 const FeedbackModel = mongoose.model("feedbacks", feedbackschema);
+const NewWebsiteModel = mongoose.model("HeroTexts", newWebsiteschema);
 
 const addToMongoose = async (data) => {
   try {
@@ -36,7 +68,7 @@ const addToMongoose = async (data) => {
       Email: data.Email,
       Phone: data.Phone,
       RefrrelCodeApplied: data.RefrrelCodeApplied,
-      ReferredBy:data.ReferredBy,
+      ReferredBy: data.ReferredBy,
       Time: data.Time,
       VPA: data.VPA,
       PTime: data.PTime,
@@ -51,7 +83,7 @@ const findbyQueryMongoose = async (query) => {
   try {
     const studentwithgivenQuery = await StudentsModel.findOne(query)
     if (studentwithgivenQuery) {
-      console.log("Correct Referral Code Applied-",query.UserRefrralCode," Refered by:",studentwithgivenQuery.FirstName, studentwithgivenQuery.LastName);
+      console.log("Correct Referral Code Applied-", query.UserRefrralCode, " Refered by:", studentwithgivenQuery.FirstName, studentwithgivenQuery.LastName);
       return studentwithgivenQuery;
     } else {
       console.log(query.UserRefrralCode, "is not a valid Referral Code");
@@ -78,16 +110,16 @@ const findallStudentsMongoose = async () => {
   }
 }
 
-const findallFeedbackMongoose = async()=>{
+const findallFeedbackMongoose = async () => {
   try {
-    const allfeedbacks=await FeedbackModel.find({});
-    if(allfeedbacks){
+    const allfeedbacks = await FeedbackModel.find({});
+    if (allfeedbacks) {
       console.log("All feedbacks fetched successfully");
       return allfeedbacks;
-    }else{
+    } else {
       console.log("There are no feedbacks");
     }
-    
+
   } catch (error) {
     console.log(error);
   }
@@ -125,12 +157,129 @@ const addFeedbackToMongoose = async (data) => {
   }
 }
 
-module.exports = { 
-  StudentsModel, 
-  addToMongoose, 
-  findbyQueryMongoose, 
-  updateToMongoose, 
+
+
+// New Website db
+
+const addNewWebsiteToMongoose = async (data) => {
+  try {
+    var new_website = new NewWebsiteModel({
+      websiteName: data.websiteName,
+      isActive: false,
+      heroT1U: data.heroT1U,
+      heroT1D: data.heroT1D,
+      heroT2U: data.heroT2U,
+      heroT2D: data.heroT2D,
+      date: data.date,
+      time: data.time,
+      price: data.price,
+      strikePrice: data.strikePrice,
+      timer: data.timer,
+      wthc1: data.wthc1,
+      wthc2: data.wthc2,
+      wttp:data.wttp,
+      Wtdp1h: data.wtdp1h,
+      wtdp1c: data.wtdp1c,
+      Wtdp2h: data.wtdp2h,
+      wtdp2c: data.wtdp2c,
+      Wtdp3h: data.wtdp3h,
+      wtdp3c: data.wtdp3c,
+      Wtdp4h: data.wtdp4h,
+      wtdp4c: data.wtdp4c,
+      Wtdp5h: data.wtdp5h,
+      wtdp5c: data.wtdp5c,
+      Wtdp6h: data.wtdp6h,
+      wtdp6c: data.wtdp6c,
+
+    })
+    await new_website.save();
+    console.log(new_website);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAllWebsites = async () => {
+  try {
+    const allwebsites = await NewWebsiteModel.find({});
+    if (allwebsites) {
+      console.log("Found all websites");
+      return allwebsites;
+    }
+    else {
+      console.log("There are no websites")
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getDesignbyQuery = async (query) => {
+  try {
+    const desingforQuery = await NewWebsiteModel.findOne(query);
+    if (desingforQuery) {
+      console.log("Found desing for the website");
+      return desingforQuery;
+    }
+    else {
+      console.log("No design found for the given website")
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateWeb = async (query, newdata) => {
+  try {
+    const updatedWeb = await NewWebsiteModel.findOneAndUpdate(query, newdata, { new: true });
+    if (updatedWeb) {
+      console.log("Website found and updated successfully");
+    }
+    else {
+      console.log("Website Not found");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const activateNewWeb = async (query, newdata) => {
+  try {
+    await NewWebsiteModel.updateMany({ isActive: true }, { isActive: false }, { new: true });
+    const updatedWeb = await NewWebsiteModel.findOneAndUpdate(query, newdata, { new: true });
+    if (updatedWeb) {
+      console.log("New website activated and previous websites deactivated");
+    }
+    else {
+      console.log("Website not found");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const deleteWeb = async (query) => {
+  try {
+    await NewWebsiteModel.deleteOne(query);
+    console.log("Delete website designs successfully")
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  StudentsModel,
+  addToMongoose,
+  findbyQueryMongoose,
+  updateToMongoose,
   addFeedbackToMongoose,
   findallStudentsMongoose,
-  findallFeedbackMongoose
+  findallFeedbackMongoose,
+  addNewWebsiteToMongoose,
+  getAllWebsites,
+  getDesignbyQuery,
+  updateWeb,
+  activateNewWeb,
+  deleteWeb,
 };
--- CodeTalk/Routes.js
+++ CodeTalk/Routes.js
@@ -1,11 +1,17 @@
 const express = require('express')
-const { 
-    addToMongoose, 
-    findbyQueryMongoose, 
-    updateToMongoose, 
+const {
+    addToMongoose,
+    findbyQueryMongoose,
+    updateToMongoose,
     addFeedbackToMongoose,
     findallStudentsMongoose,
-    findallFeedbackMongoose
+    findallFeedbackMongoose,
+    addNewWebsiteToMongoose,
+    getAllWebsites,
+    getDesignbyQuery,
+    deleteWeb,
+    updateWeb,
+    activateNewWeb,
 } = require('./Model/db');
 
 
@@ -15,12 +21,11 @@ userRouter = express.Router();
 userRouter.get('/referral/:query', async (req, res) => {
     try {
         const query = req.params.query;
-        console.log("Ye hai apki query",query);
-        const queryObj={"UserRefrralCode":query};
-        let studentbyQuery= await findbyQueryMongoose(queryObj);
-        if(studentbyQuery){
-            res.status(200).send({"Code":studentbyQuery.UserRefrralCode, "ReferredBy":studentbyQuery.FirstName+" "+studentbyQuery.LastName});
-        }else{
+        const queryObj = { "UserRefrralCode": query };
+        let studentbyQuery = await findbyQueryMongoose(queryObj);
+        if (studentbyQuery) {
+            res.status(200).send({ "Code": studentbyQuery.UserRefrralCode, "ReferredBy": studentbyQuery.FirstName + " " + studentbyQuery.LastName });
+        } else {
             res.status(404).send("This is not a valid referral code");
             console.log("Invalid referral Code Applied")
         }
@@ -33,22 +38,22 @@ userRouter.get('/referral/:query', async (req, res) => {
 
 })
 
-userRouter.get('/admin/allStudents/', async (req,res)=>{
+userRouter.get('/admin/allStudents/', async (req, res) => {
     try {
-        let allStuds=await findallStudentsMongoose();
+        let allStuds = await findallStudentsMongoose();
         res.status(200).send(allStuds);
-        
+
     } catch (error) {
         console.log(error);
         res.status(500).send("Internal server error while getting all Registered Students");
     }
 })
 
-userRouter.get('/admin/allFeedbacks/',async (req,res)=>{
+userRouter.get('/admin/allFeedbacks/', async (req, res) => {
     try {
-        let allFeeds=await findallFeedbackMongoose();
+        let allFeeds = await findallFeedbackMongoose();
         res.status(200).send(allFeeds);
-        
+
     } catch (error) {
         console.log(error);
         res.status(500).send("Internal Server Error while getting all Feedbacks")
@@ -60,7 +65,6 @@ userRouter.get('/admin/allFeedbacks/',async (req,res)=>{
 
 userRouter.post('/register/', (req, res) => {
     try {
-        // console.log("The new user data is: ", req.body);
         addToMongoose(req.body);
         res.status(200).send("YD forms has requested a post method from reactjs");
     } catch (error) {
@@ -83,16 +87,139 @@ userRouter.put('/register/checkout/', async (req, res) => {
     }
 });
 
+
+
 userRouter.post('/feedback/', async (req, res) => {
 
     try {
         await addFeedbackToMongoose(req.body);
-        res.status(200).send("Feedback added successfully")
+        res.status(200).send("Feedback added successfully");
     } catch (error) {
         console.log(error);
         res.status(500).send("Error while posting new feedback");
     }
+});
+
+//Design routes
+
+userRouter.get('/allwebsites', async (req, res) => {
+    try {
+        let allwebs = await getAllWebsites();
+        res.status(200).send(allwebs);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error")
+    }
+});
+
+userRouter.get('/design/:query', async(req,res)=>{
+    try {
+        const query =req.params.query;
+        const queryobj={'websiteName':query};
+        const design=await getDesignbyQuery(queryobj);
+        if(design){
+            console.log("Design Fetched Successfully");
+            res.status(200).send(design);
+        }
+        else{
+            console.log("Website Not Available");
+            res.status(400).send("Website not found");
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error")
+    }
+});
+
+userRouter.get('/update-website/:query', async(req,res)=>{
+    try {
+        const query =req.params.query;
+        const queryobj={'_id':query};
+        const design=await getDesignbyQuery(queryobj);
+        if(design){
+            console.log("Design Fetched Successfully");
+            res.status(200).send(design);
+        }
+        else{
+            console.log("Website Not Available");
+            res.status(400).send("Website not found");
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error")
+    }
+});
+
+userRouter.get('/active-website/', async(req,res)=>{
+    try {
+        const queryobj={'isActive':true};
+        const design=await getDesignbyQuery(queryobj);
+        if(design){
+            console.log("Design Fetched Successfully");
+            res.status(200).send(design);
+        }
+        else{
+            console.log("Website Not Available");
+            res.status(400).send("Website not found");
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error")
+    }
+});
+
+userRouter.put('/active-website/:query', async(req,res)=>{
+    try {
+        const query =req.params.query;
+        const newdata=req.body;
+        const queryobj={'_id':query};
+        const design=await activateNewWeb(queryobj,newdata);
+        res.status(200).send("Website activated successfully");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error")
+    }
+});
+
+userRouter.put('/update-website/:query', async(req,res)=>{
+    try {
+        const query =req.params.query;
+        const newdata=req.body;
+        const queryobj={'_id':query};
+        const design=await updateWeb(queryobj,newdata);
+        res.status(200).send("Website Updated Successfully");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error")
+    }
+});
+
+
+userRouter.post('/design/', async (req, res) => {
+    try {
+        await addNewWebsiteToMongoose(req.body);
+        res.status(200).send("New website data added successfully");
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error while adding new website details");
+    }
+});
+
+userRouter.delete('/deleteWebD', async (req,res)=>{
+    try {
+        const query=req.body;
+        await deleteWeb(query)
+    } catch (error) {
+        
+    }
 })
+
+
+
+
+
+
 
 
 
