5fe2c1a76a101befa91122a8eecaba25532bd22a
Adi <mishraadi733@gmail.com>
2023-12-17 17:55:11 +0530

    rate-limit

--- CodeTalk/app.js
+++ CodeTalk/app.js
@@ -5,7 +5,16 @@ const config=require('./config')
 const PORT =config.PORT;
 const connectToMongoose=require('./Model/mongooseConnection')
 const {userRouter}=require('./Routes')
+const rateLimit = require('express-rate-limit');
 
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, //15 minutes
+	limit: 100, //Limit each IP to 100 requests per seconds
+})
+
+
+app.use(limiter)
 
 
 connectToMongoose();
